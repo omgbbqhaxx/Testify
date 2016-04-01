5cddbef04fc3e865f33a9910fb0dc0f26890d499
Çağdaş Atsüren <cagdasatsuren@cagdas-mac-mini.local>
2016-04-01 14:27:08 +0300

    Initial Commit

---
+++ Testify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testify
+//
+//  Created by Çağdaş Atsüren on 1.04.2016.
+//  Copyright © 2016 Çağdaş Atsüren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
